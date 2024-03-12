72519994a0e0cc4b19ae24edeb62319a8b849394
Giovanni <giomail33@gmail.com>
2024-03-12 02:09:24 -0400

    created basic title and paragraph buttons, note:changing requested elements can be done by passing in different button paramaters in JSX

---
+++ website-builder/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+function App() {
+  
+const addElement = (elementTag) => {
+  console.log(`${elementTag}`);
+  const elements = document.querySelector('.elements');
+  const newElement = document.createElement(`${elementTag}`);
+  newElement.innerHTML = 'Type here';
+  newElement.classList.add(`${elementTag}Element`);
+  elements.appendChild(newElement);
+}
+
+  return (
+    <div className="App">
+      <div className="Main">
+        <div className='Element-Buttons'>
+          <button className='title-btn' onClick={() => addElement('h1')}>Title</button>
+          <button className='paragraph-btn' onClick={() => addElement('p')}>Paragraph</button>
+        </div>
+        <div className='elements'></div> 
+      </div>      
+    </div>
+  );
+}
+
+export default App;
